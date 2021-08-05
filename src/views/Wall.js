63ff10c5fe54f73d4b31268b212cd71051084ca8
Lucas <dexterfail@hotmail.com>
2021-08-05 11:06:54 -0300

    wall screen complete

--- src/views/Wall.js
+++ src/views/Wall.js
@@ -8,6 +8,14 @@ import {
   CCardBody,
   CDataTable,
   CButtonGroup,
+  CModal,
+  CModalHeader,
+  CModalBody,
+  CModalFooter,
+  CFormGroup,
+  CLabel,
+  CInput,
+  CTextarea,
 } from "@coreui/react";
 import CIcon from "@coreui/icons-react";
 import useApi from "../services/api";
@@ -17,11 +25,20 @@ export default function Wall() {
 
   const [loading, setLoading] = useState(true);
   const [list, setList] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [modalLoading, setModalLoading] = useState(false);
+  const [modalTitleField, setModalTitleField] = useState("");
+  const [modalBodyField, setModalBodyField] = useState("");
+  const [modalId, setModalId] = useState("");
 
   const fields = [
     { label: "Título", key: "title" },
-    { label: "Data de Criação", key: "datecreated", _style:{width:'200px'} },
-    { label: "Ações", key: "actions", _style:{width:'1px'} },
+    {
+      label: "Data de Criação",
+      key: "datecreated",
+      _style: { width: "200px" },
+    },
+    { label: "Ações", key: "actions", _style: { width: "1px" } },
   ];
 
   useEffect(() => {
@@ -39,41 +56,155 @@ export default function Wall() {
     }
   };
 
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  const handleEditButton = (index) => {
+    setModalId(list[index]["id"]);
+    setModalTitleField(list[index]["title"]);
+    setModalBodyField(list[index]["body"]);
+    setShowModal(true);
+  };
+
+  const handleRemoveButton = async (index) => {
+    if (window.confirm("Tem certeza que deseja excluir?")) {
+      const result = await api.removeWall(list[index]["id"]);
+      if (result.error === "") {
+        getList();
+      } else {
+        alert(result.error);
+      }
+    }
+  };
+
+  const handleNewButton = () => {
+    setModalId("");
+    setModalTitleField("");
+    setModalBodyField("");
+    setShowModal(true);
+  };
+
+  const handleModalSave = async () => {
+    if (modalTitleField && modalBodyField) {
+      setModalLoading(true);
+      let result;
+      let data = {
+        title: modalTitleField,
+        body: modalBodyField,
+      };
+
+      if (modalId === "") {
+        result = await api.addWall(data);
+      } else {
+        result = await api.updateWall(modalId, data);
+      }
+
+      setModalLoading(false);
+      if (result.error === "") {
+        setShowModal(false);
+        getList();
+      } else {
+        alert(result.error);
+      }
+    } else {
+      alert("Preencha os campos!");
+    }
+  };
+
   return (
-    <CRow>
-      <CCol>
-        <h2>Mural de Avisos</h2>
-        <CCard>
-          <CCardHeader>
-            <CButton color="primary">
-              <CIcon name="cil-check" />
-              Novo Aviso
-            </CButton>
-          </CCardHeader>
-          <CCardBody>
-            <CDataTable
-              items={list}
-              fields={fields}
-              loading={loading}
-              noItemsViewSlot=" "
-              hover
-              striped
-              bordered
-              pagination
-              itemsPerPage={5}
-              scopedSlots={{
-                "actions": (item, index) => 
-                <td>
-                    <CButtonGroup>
-                        <CButton color="info">Editar</CButton>
-                        <CButton color="danger">Excluir</CButton>
-                    </CButtonGroup>
-                </td>,
-              }}
+    <>
+      <CRow>
+        <CCol>
+          <h2>Mural de Avisos</h2>
+          <CCard>
+            <CCardHeader>
+              <CButton color="primary" onClick={handleNewButton}>
+                <CIcon name="cil-check" />
+                Novo Aviso
+              </CButton>
+            </CCardHeader>
+            <CCardBody>
+              <CDataTable
+                items={list}
+                fields={fields}
+                loading={loading}
+                noItemsViewSlot=" "
+                hover
+                striped
+                bordered
+                pagination
+                itemsPerPage={5}
+                scopedSlots={{
+                  actions: (item, index) => (
+                    <td>
+                      <CButtonGroup>
+                        <CButton
+                          color="info"
+                          onClick={() => handleEditButton(index)}
+                        >
+                          Editar
+                        </CButton>
+                        <CButton
+                          color="danger"
+                          onClick={() => handleRemoveButton(index)}
+                        >
+                          Excluir
+                        </CButton>
+                      </CButtonGroup>
+                    </td>
+                  ),
+                }}
+              />
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
+
+      <CModal show={showModal} onClose={handleCloseModal}>
+        <CModalHeader closeButton>
+          {modalId === "" ? "Novo" : "Editar"} Aviso
+        </CModalHeader>
+        <CModalBody>
+          <CFormGroup>
+            <CLabel htmlFor="modal-title">Titulo do Aviso</CLabel>
+            <CInput
+              type="text"
+              id="modal-title"
+              placeholder="Digite um título para o aviso"
+              value={modalTitleField}
+              onChange={(e) => setModalTitleField(e.target.value)}
+              disabled={modalLoading}
             />
-          </CCardBody>
-        </CCard>
-      </CCol>
-    </CRow>
+          </CFormGroup>
+          <CFormGroup>
+            <CLabel htmlFor="modal-body">Corpo do Aviso</CLabel>
+            <CTextarea
+              id="modal-body"
+              placeholder="Digite o conteudo do aviso"
+              value={modalBodyField}
+              onChange={(e) => setModalBodyField(e.target.value)}
+              disabled={modalLoading}
+            />
+          </CFormGroup>
+        </CModalBody>
+        <CModalFooter>
+          <CButton
+            color="primary"
+            onClick={handleModalSave}
+            disabled={modalLoading}
+          >
+            {modalLoading ? "Carregando..." : "Salvar"}
+          </CButton>
+          <CButton
+            color="secondary"
+            onClick={handleCloseModal}
+            disabled={modalLoading}
+          >
+            {modalLoading ? "Carregando..." : "Cancelar"}
+          </CButton>
+        </CModalFooter>
+      </CModal>
+    </>
   );
 }
