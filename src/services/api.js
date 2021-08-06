98073cacf93d5731894e2dfa8d2564aac4f29ca9
Lucas <dexterfail@hotmail.com>
2021-08-06 09:13:25 -0300

    Create reservations page

--- src/services/api.js
+++ src/services/api.js
@@ -122,5 +122,24 @@ export default function services() {
       let json = await request("delete", `/doc/${id}`, {}, token);
       return json;
     },
+
+    getReservations: async () => {
+      let token = localStorage.getItem("token");
+      let json = await request("get", "/reservations", {}, token);
+      return json;
+    },
+
+    getUnits: async () => {
+      let token = localStorage.getItem("token");
+      let json = await request("get", "/units", {}, token);
+      return json;
+    },
+
+    getAreas: async () => {
+      let token = localStorage.getItem("token");
+      let json = await request("get", "/areas", {}, token);
+      return json;
+    },
+
   };
 }
